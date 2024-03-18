56cfb0c8d5c63e56fd040a6d6357f78615b01afa
VirLg <108211942+VirLg@users.noreply.github.com>
2024-03-18 23:36:40 +0300

    refacroring

--- js/index.js
+++ js/index.js
@@ -1,51 +1,32 @@
 let positionX = 0;
-
-const refLeftBtn = document.querySelector('.leftBtn');
-
 window.addEventListener('resize', function () {
   redDiv.style.left = '40px';
   refLeftBtn.disabled = true;
 });
+const ref={
+  refLeftBtn:document.querySelector('.leftBtn'),
+  refHero :document.querySelector('#redDiv'),
+  refRightBtn : document.querySelector('.rightBtn'),
 
-const refHero = document.querySelector('#redDiv');
-const refRightBtn = document.querySelector('.rightBtn');
-
-refLeftBtn.addEventListener('click', onToLeftBtnClick);
-
+}
+ref.refRightBtn.style.marginTop = `${ ref.refHero.getBoundingClientRect().height + 15}px`;
+ref.refLeftBtn.style.marginTop = `${refHero.getBoundingClientRect().height + 15}px`;
 function positionHero() {
-  positionX = refHero.getBoundingClientRect().x;
+  positionX = ref.refHero.getBoundingClientRect().x;
 }
-
-function onToLeftBtnClick(evt) {
-  positionHero();
+ref.refLeftBtn.addEventListener('click', ()=>{
+    positionHero();
   redDiv.style.left = positionX - 88 + 'px';
-  console.log('first', positionX);
-  if (positionX < 129) refLeftBtn.disabled = true;
-  if (positionX < 1210) refRightBtn.disabled = false;
-}
-refRightBtn.style.marginTop = `${
-  refHero.getBoundingClientRect().height + 15
-}px`;
-
-refLeftBtn.style.marginTop = `${refHero.getBoundingClientRect().height + 15}px`;
-
-refRightBtn.addEventListener('click', onToRightBtnClick);
-function onToRightBtnClick(evt) {
+  if (positionX < 129) ref.refLeftBtn.disabled = true;
+  if (positionX < 1210) ref.refRightBtn.disabled = false;
+});
+ref.refRightBtn.addEventListener('click', ()=>{
   positionHero();
   redDiv.style.left = positionX + 88 + 'px';
-  console.log('first', positionX);
-  if (positionX > 7) refLeftBtn.disabled = false;
-  if (positionX > 980) refRightBtn.disabled = true;
-}
+  if (positionX > 7) ref.refLeftBtn.disabled = false;
+  if (positionX > 980) ref.refRightBtn.disabled = true;
+});
 
-window.addEventListener('keydown', onKeypress);
 
-function onKeypress(e) {
-  if (e.key === 'ArrowRight') console.log('e', e);
-  let left = e.clientX;
-}
 
-function moveAt(pageX, pageY) {
-  ball.style.left = pageX - shiftX + 'px';
-  ball.style.top = pageY - shiftY + 'px';
-}
+
